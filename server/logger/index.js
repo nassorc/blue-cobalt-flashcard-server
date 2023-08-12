2041f8f79b65b462e088367e71b2d311fc73ccec
nassorc <macrossan17@gmail.com>
2023-08-12 00:46:05 -0700

    Replace logger module

--- server/logger/index.js
+++
@@ -1,7 +0,0 @@
-const productionLogger = require('./productionLogger')();
-const developmentLogger = require('./developmentLogger')();
-
-module.exports = {
-    productionLogger,
-    developmentLogger,
-} 
