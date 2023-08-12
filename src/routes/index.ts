8a9e18b88fdc282317ed51ef8a63185bd02d8bc6
nassorc <macrossan17@gmail.com>
2023-08-12 03:18:21 -0700

    Add a catch all route to return a 404 response to the client

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,12 @@
 import deckRoute from '../app/deck/deckRoute'
 import userRoute from '../app/user/userRoute'
 import validateToken from '../middleware/validateToken'
+import AppError from '../lib/error/AppError'
 
 module.exports = (app: any) => {
     app.use('/deck', validateToken, deckRoute)
     app.use('/user', userRoute)
+    app.all('*', (req, res, next) => {
+      next(new AppError('This route does not exist', 404))
+    })
 }
