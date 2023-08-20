ad592a207ff6846d3cc54c4ffe86164b2537a8b5
nassorc <macrossan17@gmail.com>
2023-08-19 21:58:07 -0700

    Add a default export for the config directory  containing configuration settings for the server
    The config file includes global variables used to for configuring the server.
    This commit includes variables for handling the authentication token.

--- src/config/httpStatus.ts
+++ src/config/httpStatus.ts
@@ -2,7 +2,8 @@ const httpStatus = {
   SUCCESS: 200,
   CREATED: 201,
   BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
+  UNAUTHORIZED: 401, // lacks valid credentials
+  FORBIDDEN: 403, // valid credentials, but lacks the permissions
   NOT_FOUND: 404,
   CONFLICT: 409,
   SERVER_ERROR: 500
